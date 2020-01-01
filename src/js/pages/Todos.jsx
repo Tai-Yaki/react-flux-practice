50da0a1c503c9a9944901032ae39167a7f741402
Taiki Yamada <t.yamada@chapter-5.com>
2020-01-01 23:55:43 +0900

    storeにcreateTodoメソッドを追加

--- src/js/pages/Todos.jsx
+++ src/js/pages/Todos.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Todo from '../components/Todo';
 import TodoStore from '../stores/TodoStore';
 
 const Todos = () => {
-  const todos = TodoStore.getAll();
+  const [todos, setTodos] = useState(TodoStore.getAll());
+
+  useEffect(() => {
+    TodoStore.on('change', () => {
+      setTodos(TodoStore.getAll());
+    });
+  }, []);
 
   const TodoList = todos.map((todo) => (
     <Todo key={todo.id} text={todo.text} complete={todo.complete} />
