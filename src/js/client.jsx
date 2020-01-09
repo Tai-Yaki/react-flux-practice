d31dac6803123469830e645a38a7fdb499a2cfa2
Taiki Yamada <t.yamada@chapter-5.com>
2020-01-09 23:59:01 +0900

    ページ遷移を追加

--- src/js/client.jsx
+++ src/js/client.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Todos from './pages/Todos';
+import Favorites from './pages/Favorites';
 import Layout from './pages/Layout';
 
 const app = document.getElementById('app');
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Router>
       <Layout>
         <Route exact path="/" component={Todos} />
+        <Route exact path="/favorites" component={Favorites} />
       </Layout>
     </Router>
   </div>,
